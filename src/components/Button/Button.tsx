352b149dfb119a515db99fbfe96fcd670f56f248
denisesleite <denisegd01@gmail.com>
2022-10-18 00:38:40 -0300

    refact

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,24 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
-import { ReactNode } from 'react';
 
-export interface ButtonProps {
-  size?: 'sm' | 'md' | 'lg';
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  asChild: boolean;
+  asChild?: boolean;
 }
 
-export function Button({ size = 'md', children, asChild }: ButtonProps){
-  const ButtonComponent = asChild ? Slot : 'h2';
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
+  const Comp = asChild ? Slot : 'button';
 
   return (
-    <ButtonComponent className={clsx(
-      'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white'
-    )}
+    <Comp 
+      className={clsx(
+        'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
+        className,
+      )}
+      {...props}
     >
       {children}
-    </ButtonComponent>
+    </Comp>
   )
 }
--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,6 +1,6 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { Text } from '../Text/Text';
+import { Meta, StoryObj } from '@storybook/react'
 import { Checkbox, CheckboxProps } from './Checkbox'
+import { Text } from '../Text/Text'
 
 export default {
   title: 'Components/Checkbox',
@@ -10,13 +10,13 @@ export default {
   decorators: [
     (Story) => {
       return (
-        <div className='flex items-center gap-2'>
+        <div className="flex items-center gap-2">
           {Story()}
-          <Text size='sm'>Lembrar-me de mim por 30 dias</Text>
+          <Text size="sm">Lembrar-me de mim por 30 dias</Text>
         </div>
       )
-    } 
-  ]
+    }
+  ],
 } as Meta<CheckboxProps>
 
 export const Default: StoryObj<CheckboxProps> = {}
